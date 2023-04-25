749c55d5aa8628b0fea3bf532ef0425ce7ca538a
yadavgaurav955 <131779590+yadavgaurav955@users.noreply.github.com>
2023-04-25 19:08:09 +0530

    Update streamlit_app.py
    added header and text code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Dinner")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("kale, spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
