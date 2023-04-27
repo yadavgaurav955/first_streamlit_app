aebf63f6d1cfffcc1383c1e92091bc703ed58287
yadavgaurav955 <131779590+yadavgaurav955@users.noreply.github.com>
2023-04-27 19:54:39 +0530

    Update streamlit_app.py
    remove text line

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + "kiwi")
 
 streamlit.header("Fruityvice Fruit Advice!")
 
