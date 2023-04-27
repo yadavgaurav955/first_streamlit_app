fb91c08a002b8a0a9c98580129d1ce25e8a530dc
yadavgaurav955 <131779590+yadavgaurav955@users.noreply.github.com>
2023-04-27 20:08:23 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + 'fruit_choice')
 
 streamlit.header("Fruityvice Fruit Advice!")
 
