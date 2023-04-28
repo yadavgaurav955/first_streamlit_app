948d2950d24a362cc6eeeac8c38368cfe77c4749
yadavgaurav955 <131779590+yadavgaurav955@users.noreply.github.com>
2023-04-28 19:38:13 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,5 +49,5 @@ streamlit.dataframe(my_data_rows)
 
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding jackfruit', add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
 
